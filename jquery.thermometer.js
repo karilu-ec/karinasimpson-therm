5b6ea4e53ddc4dcfaa4675e43922a80421132c18
John Vargo <jbrodiev@gmail.com>
2012-01-29 00:18:21 -0500

    Revised to separate a jQuery and non-jQuery version.  Added example HTML files

---
+++ jquery.thermometer.js
@@ -0,0 +1,54 @@
+/**
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * To use this, you need to include gauge.js, jquery, and this file:
+ *
+ *   <script type="text/javascript" src="../jquery-1.6.min.js"></script>
+ *   <script type="text/javascript" src="../jquery.thermometer.js"></script>
+ *   <script type="text/javascript" src="../thermometer.js"></script>
+ *
+ *
+ */
+(function( $ ){
+  var methods = {
+    init : function( options ) {
+   
+        return this.each(function(){
+            var therm = new ThermometerGuage( $(this)[0], options );
+            $(this).data('therm', therm);
+        });    
+       
+    },
+    setValue : function( value ) {
+        return this.each(function(){
+            var therm = $(this).data('therm');
+            if (therm != null) {        
+                therm.setValue( value );
+            }
+        });
+    } //,
+    //draw : function( value ) {  
+    //    return this.each(function(){
+    //        var therm = $(this).data('therm');
+    //        if (therm != null) {        
+    //            therm.draw(value);
+    //        }
+    //    });
+    //}
+
+  };
+
+  $.fn.thermometer = function( method ) {
+   
+    // Method calling logic
+    if ( methods[method] ) {
+      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.thermometer' );
+    }    
+ 
+  };
+})( jQuery );
